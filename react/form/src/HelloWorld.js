27868d203ea48e8ace3d9c5677b54d15c6387554
adrianmunoz <adrian.munoz@codurance.com>
2023-04-23 10:43:58 +0200

    chore: removed HelloWorld component

--- react/form/src/HelloWorld.js
+++
@@ -1,3 +0,0 @@
-export function HelloWorld() {
-  return <p>hello world</p>;
-}
--- react/form/src/index.js
+++ react/form/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelloWorld } from './HelloWorld';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
   </React.StrictMode>
 );
