9f6a7aff607820531f09de8a36befe076202300f
adrianmunoz <adrian.munoz@codurance.com>
2023-04-24 08:17:39 +0200

    feature: added main element and import styles

--- react/form/src/index.js
+++ react/form/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <main>
+    </main>
   </React.StrictMode>
 );
