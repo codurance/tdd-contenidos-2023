832c46448edeb1945341e5fb882c8f338bcd66cc
adrianmunoz <adrian.munoz@codurance.com>
2023-05-09 08:14:10 +0200

    feature: added PasswordForm to entry point

--- react/form/src/index.js
+++ react/form/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
+import {PasswordForm} from "./PasswordForm";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <main>
+        <PasswordForm />
     </main>
   </React.StrictMode>
 );
