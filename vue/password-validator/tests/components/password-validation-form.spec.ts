c03801ef4c4ea3a44021610ff7cb07ede7578f26
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-05-16 16:17:47 +0200

    RED: should validate a too short password

--- vue/password-validator/tests/components/password-validation-form.spec.ts
+++ vue/password-validator/tests/components/password-validation-form.spec.ts
@@ -1,9 +1,21 @@
 import PasswordValidatorForm from '@/components/PasswordValidatorForm.vue'
 import { render } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event';
 
 describe('Password validator Form', () => {
   it('should have a button', () => {
     const { getByText } = render(PasswordValidatorForm)
     expect(getByText('Enviar consulta')).toBeInTheDocument()
+  })
+
+  it('should validate a too short password', async () => {
+    const { getByPlaceholderText, getByText } = render(PasswordValidatorForm)
+    const input = getByPlaceholderText('Pon tú contraseña')
+    expect(input).toBeInTheDocument()
+
+    await userEvent.keyboard("short1")
+    await userEvent.click(getByText('Enviar consulta'))
+
+    expect(getByText('The password should have a length of 8')).toBeInTheDocument()
   })
 })
