410cc27e613c37ceafa277790180bb8f8f73173f
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-05-25 10:14:14 +0200

    chore: setup again recording 99999

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -1,2 +1,17 @@
+import {render} from "@testing-library/angular";
+import {ReactiveFormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
+
+function buildComponent() {
+  return render(AppComponent, {
+    imports: [ReactiveFormsModule]
+  });
+}
+
 describe('Password validation kata', () => {
+  it('should render title', async () => {
+    const { findByText } = await buildComponent();
+
+    expect(await findByText('my text')).toBeInTheDocument();
+  })
 });
