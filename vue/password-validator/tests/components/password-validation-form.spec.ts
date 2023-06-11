652bbf355e14f9fd211f2fd36a4722ee53da656b
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-06-11 13:59:57 +0200

    RED: should show the error of short password when the password is {short1}

--- vue/password-validator/tests/components/password-validation-form.spec.ts
+++ vue/password-validator/tests/components/password-validation-form.spec.ts
@@ -1,10 +1,21 @@
 import { render } from '@testing-library/vue'
 import PasswordValidatorForm from '@/components/PasswordValidatorForm.vue'
+import userEvent from '@testing-library/user-event'
 
 describe('Password validator form', () => {
   it('should have a button and input to validate the password', () => {
     const { getByPlaceholderText, getByText } = render(PasswordValidatorForm)
     expect(getByPlaceholderText('Introduce tu contraseña')).toBeInTheDocument()
     expect(getByText('Enviar consulta')).toBeInTheDocument()
+  })
+
+  it('should show the error of short password when the password is {short1}', async () => {
+    const { getByPlaceholderText, getByText } = render(PasswordValidatorForm)
+
+    await userEvent.click(getByPlaceholderText('Introduce tu contraseña'))
+    await userEvent.keyboard('short1')
+    await userEvent.click(getByText('Enviar consulta'))
+
+    expect(getByText('The password should have length of 8')).toBeInTheDocument()
   })
 })
