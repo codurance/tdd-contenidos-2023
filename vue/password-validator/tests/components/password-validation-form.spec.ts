e72437e7a28b09475105b2100855335c92c31de8
S-DRE <sdreworks@gmail.com>
2023-06-12 12:01:30 +0200

    RED: should show an error when password is too short

--- vue/password-validator/tests/components/password-validation-form.spec.ts
+++ vue/password-validator/tests/components/password-validation-form.spec.ts
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/vue'
 import PasswordValidatorForm from '@/components/PasswordValidatorForm.vue'
+import userEvent from '@testing-library/user-event'
 
 describe('Password validator form', () => {
   it('should have a button and an input', () => {
@@ -7,5 +8,15 @@ describe('Password validator form', () => {
 
     expect(getByPlaceholderText('Introduce tu contraseña')).toBeInTheDocument()
     expect(getByText('Enviar consulta')).toBeInTheDocument()
+  })
+
+  it('should show an error when password is too short', async () => {
+    const { getByPlaceholderText, getByText } = render(PasswordValidatorForm)
+
+    await userEvent.click(getByPlaceholderText('Introduce tu contraseña'))
+    await userEvent.keyboard('short1')
+    await userEvent.click(getByText('Enviar consulta'))
+
+    expect(getByText('The password should have length of 8')).toBeInTheDocument()
   })
 })
