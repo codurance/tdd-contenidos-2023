97ba78a0a4f955a8a5eeeb7ce5de6a1f3115b00f
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-06-12 11:53:39 +0200

    RED: should have a button and an input

--- vue/password-validator/tests/components/password-validation-form.spec.ts
+++ vue/password-validator/tests/components/password-validation-form.spec.ts
@@ -2,7 +2,10 @@ import { render } from '@testing-library/vue'
 import PasswordValidatorForm from '@/components/PasswordValidatorForm.vue'
 
 describe('Password validator form', () => {
-  it('should have a button', () => {
-    expect(true).toBe(true)
+  it('should have a button and an input', () => {
+    const { getByPlaceholderText, getByText } = render(PasswordValidatorForm)
+
+    expect(getByPlaceholderText('Introduce tu contraseña')).toBeInTheDocument()
+    expect(getByText('Enviar consulta')).toBeInTheDocument()
   })
 })
